8241aa502fbfe4565dd5aa90484ce6093f51b4f5
Kirill Elagin <kirelagin@gmail.com>
2013-04-29 03:10:11 +0400

    Abstract Worker
    Clses #8

---
+++ src/itmo/evaluator/AbstractWorker.java
@@ -0,0 +1,64 @@
+package itmo.evaluator;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import itmo.dreamq.MessageQueue;
+import itmo.mq.Envelope;
+import itmo.mq.Message;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+/**
+ * @author Vladislav Kononov vincent@yandex-team.ru
+ */
+public abstract class AbstractWorker {
+
+    public static final int TIME_OUT = 1000;
+
+    private int tag;
+
+    public AbstractWorker(int tag) {
+        this.tag = tag;
+    }
+
+    public void run() throws MalformedURLException, InvalidProtocolBufferException, InterruptedException {
+        URL url = new URL("http://localhost:9999/mq?wsdl");
+        QName qname = new QName("http://dreamq.itmo/", "DreamQueueService");
+
+        Service service = Service.create(url, qname);
+
+        MessageQueue messageQueue = service.getPort(MessageQueue.class);
+
+        while (true) {
+            Envelope envelope = messageQueue.get(tag);
+            Message msg = envelope.getMsg();
+            if (msg != null) {
+                EvalMessage.Request e = EvalMessage.Request.parseFrom(msg.getMsg());
+                EvalMessage.Request.Operation o = e.getOp();
+                if (canPerform(o)) {
+                    int evaluator = e.getEvaluatorId();
+                    double res = perform(o, e.getArgList());
+                    EvalMessage.Reply r = EvalMessage.Reply.newBuilder()
+                            .setRes(res)
+                            .setSeq(e.getSeq())
+                            .build();
+                    Message m = new Message();
+                    m.setMsg(r.toByteArray());
+                    messageQueue.put(evaluator, m);
+                    messageQueue.ack(envelope.getTicketId());
+                    System.out.println(o.toString() + " " + e.getArgList().toString() + " = " + res);
+                }
+            } else {
+                Thread.sleep(TIME_OUT);
+            }
+        }
+    }
+
+    abstract boolean canPerform(EvalMessage.Request.Operation op);
+
+    abstract double perform(EvalMessage.Request.Operation op, List<Double> args);
+
+}
--- src/itmo/evaluator/PWorker.java
+++ src/itmo/evaluator/PWorker.java
@@ -1,63 +1,30 @@
 package itmo.evaluator;
 
-import com.google.protobuf.InvalidProtocolBufferException;
-import itmo.dreamq.MessageQueue;
-import itmo.mq.Envelope;
-import itmo.mq.Message;
-
-import javax.xml.namespace.QName;
-import javax.xml.ws.Service;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.util.List;
 
 /**
  * @author Vladislav Kononov vincent@yandex-team.ru
  */
-public class PWorker {
+public class PWorker extends AbstractWorker {
 
-    public static final int TAG = -1;
+    public PWorker() {
+        super(-1);
+    }
 
-    public static final int TIME_OUT = 1000;
+    @Override
+    boolean canPerform(EvalMessage.Request.Operation op) {
+        return op == EvalMessage.Request.Operation.ADD ||
+               op == EvalMessage.Request.Operation.SUB;
+    }
 
-
-    public static void main(String[] args) throws MalformedURLException, InvalidProtocolBufferException, InterruptedException {
-        URL url = new URL("http://localhost:9999/mq?wsdl");
-        QName qname = new QName("http://dreamq.itmo/", "DreamQueueService");
-
-        Service service = Service.create(url, qname);
-
-        MessageQueue messageQueue = service.getPort(MessageQueue.class);
-
-        while(true){
-            Envelope envelope = messageQueue.get(TAG);
-            Message msg = envelope.getMsg();
-            if (msg != null) {
-                EvalMessage.Request e = EvalMessage.Request.parseFrom(msg.getMsg());
-                EvalMessage.Request.Operation o = e.getOp();
-                if (o == EvalMessage.Request.Operation.ADD || o == EvalMessage.Request.Operation.SUB) {
-                    int tag = e.getEvaluatorId();
-                    double arg1 = e.getArg(0);
-                    double arg2 = e.getArg(1);
-                    double res;
-                    if (o == EvalMessage.Request.Operation.ADD) {
-                        res = arg2 + arg1;
-                    } else {
-                        res = arg2 - arg1;
-                    }
-                    EvalMessage.Reply r = EvalMessage.Reply.newBuilder()
-                            .setRes(res)
-                            .setSeq(e.getSeq())
-                            .build();
-                    Message m = new Message();
-                    m.setMsg(r.toByteArray());
-                    messageQueue.put(tag, m);
-                    messageQueue.ack(envelope.getTicketId());
-                    System.out.println("Result " + arg1 + " after " + e.getOp().toString());
-                }
-            } else {
-                Thread.sleep(TIME_OUT);
-            }
+    @Override
+    double perform(EvalMessage.Request.Operation op, List<Double> args) {
+        if (op == EvalMessage.Request.Operation.ADD) {
+            return args.get(0) + args.get(1);
+        } else if (op == EvalMessage.Request.Operation.SUB) {
+            return args.get(0) - args.get(1);
         }
+        throw new IllegalArgumentException("Can't perform this");
     }
 
 }
